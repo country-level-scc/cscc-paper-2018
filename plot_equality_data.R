038d94afb1045aff1ee004b302e7f442ad108df7
Lisanne <lisanne.blok19@imperial.ac.uk>
2021-06-28 12:06:07 +0200

    Added options and documentation

--- plot_equality_data.R
+++ plot_equality_data.R
@@ -1,21 +1,76 @@
-library(data.table)
 library(ggplot2)
 library(stringr)
-# Should be either "eri_eq_statscc_2020d", "poor_pref_10dollars" 
-type_str = "poor_pref_10dollars" 
-# Name the file with the version number
-version_string = "v4"
+library(docopt)
+library(data.table)
+
+# This file plots results from the generate_cscc.R file
+# The outputs can be either "eri_eq_statscc_2020d" (GSCC relative to the income of an Eritrean),
+# or "poor_pref_10dollars" (income at which donation is preferable to SCC) 
+
+# Input values in options should correspond with files in the results folder for -e, -r and -f.
+
+' Input values to be plotted. Use values also used to generate files in the generate_cscc.r results folder
+
+usage: plot_equality_data.R [-e <eta> -v <ver> -s <string> -r <rcp> -f <dmg>]
+
+options:
+ -e Eta value used in the model (1, 2 or 1:2 (default))
+ -v Version number to name the plotted figure
+ -s Type of string and plot (either poor_pref_10dollars (default) or eri_eq_statscc_2020d)
+ -r Plot rcp scenario (4.5, 6.0, 8.5 or all (default))
+ -f Damage function (default=bhm (Burke et al.), djo (Dell et al.). separate by columns)' -> my_doc
+
+#my_opts <- docopt(my_doc, "-e 1 -v v4 -s poor_pref_10dollars -r 6.0,4.5,8.5 -f bhm") # Default case
+#my_opts <- docopt(my_doc, "-e 1,2 -v v2 -s eri_eq_statscc_2020d -r 8.5 -f bhm,djo") 
+my_opts <- docopt(my_doc, "-s eri_eq_statscc_2020d -f bhm,djo") 
+#my_opts <- docopt(my_doc)
+
+# unpack variables from the options
+if (is.null(my_opts[["e"]])){ # default RRA is 1 and 2
+  variable_risk = c(1,2)
+} else if (my_opts[["e"]] == 1) {
+  variable_risk = c(1)
+} else if (my_opts[["e"]]==2){
+  variable_risk = c(2)
+} else {variable_risk = c(1,2)}
+
+if (is.null(my_opts[["v"]])) {
+  version_string = "" # no version number if there is no input for -v
+} else{ version_string = as.character(my_opts[["v"]])
+}
+
+if (is.null(my_opts[["s"]])){
+  type_str = "poor_pref_10dollars" #default
+} else{type_str = as.character(my_opts[["s"]])}  
+
+variable_rcp = c()
+if (length(grep(4.5, my_opts[["r"]]) != 0)){
+  variable_rcp <- append(variable_rcp, 45)
+} 
+if (length(grep(6.0, my_opts[["r"]]) != 0)){
+  variable_rcp <- append(variable_rcp, 60)
+} 
+if (length(grep(8.5, my_opts[["r"]])) != 0){
+  variable_rcp <- append(variable_rcp, 85)
+} 
+if (length(variable_rcp) == 0){variable_rcp = c(45,60,85)}
+
+if (is.null(my_opts[["f"]])) {
+  dmg_f = "bhm"     # bhm is default damage function
+} else {
+  dmg_f = as.character(my_opts["f"])
+}
 
 # Conversion factor
 dollar_val_2020 = 1.35
 
 namefun <- function(x, y, z, timeframe){
-    paste0("/results/res_statbhm_30C/", type_str, "SSP", x, "_rcp", y, 
-           "_", timeframe, "_estimates_climensemble_", "eta_", z, ".RData") 
+  paste0("/results/res_statbhm_30C/", type_str, "SSP", x, "_rcp", y, 
+         "_", timeframe, "_estimates_climensemble_", "eta_", z, ".RData") 
 }
 namefun_altdamage <- function(x, y, z){
   paste0("/results/res_statdjo_richpoor/", type_str, "SSP", x, "_rcp", y,
-  "_constant_estimates_climensemble_djo_", "eta_", z, ".RData")
+         "_constant_estimates_climensemble_djo_", "eta_", z, ".RData")
 }
 namefun_nocut <- function(x, y, z, timeframe){
   paste0("/results/res_statbhm/", type_str, "SSP", x, "_rcp", y, 
@@ -23,26 +78,31 @@ namefun_nocut <- function(x, y, z, timeframe){
 }
 
 filelist=c()
- 
-for (y in c(45, 60, 85)){
-  for (z in c(1, 2)){
-    filelist2 = lapply(c(1:5), namefun_altdamage, y=y, z=z)
-    filelist=c(filelist, filelist2)
-  }
-}
-for (y in c(45, 60, 85)){
-  for (z in c(1, 2)){
-    for (timeframe in c("horizon2100", "constant")){
-      filelist2 = lapply(c(1:5), namefun, y=y, z=z, timeframe=timeframe) 
+
+if (length(grep( "djo", dmg_f))!=0){
+  for (y in variable_rcp){
+    for (z in variable_risk){
+      filelist2 = lapply(c(1:5), namefun_altdamage, y=y, z=z)
       filelist=c(filelist, filelist2)
     }
   }
-}
-for (y in c(45, 60, 85)){
-  for (z in c(1, 2)){
-    for (timeframe in c("horizon2100", "constant")){
-      filelist2 = lapply(c(1:5), namefun_nocut, y=y, z=z, timeframe=timeframe) 
-      filelist=c(filelist, filelist2)
+}  
+
+if (length(grep("bhm", dmg_f))!=0){ 
+  for (y in variable_rcp){
+    for (z in variable_risk){
+      for (timeframe in c("horizon2100", "constant")){
+        filelist2 = lapply(c(1:5), namefun, y=y, z=z, timeframe=timeframe) 
+        filelist=c(filelist, filelist2)
+      }
+    }
+  }
+  for (y in variable_rcp){
+    for (z in variable_risk){
+      for (timeframe in c("horizon2100", "constant")){
+        filelist2 = lapply(c(1:5), namefun_nocut, y=y, z=z, timeframe=timeframe) 
+        filelist=c(filelist, filelist2)
+      }
     }
   }
 }
@@ -99,11 +159,11 @@ if(type_str == "eri_eq_statscc_2020d"){
   ) 
   plot_ylab = ylab("SCC (2020 USD)") 
 } else if(type_str == "poor_pref_10dollars"){
-    plot_labs = labs(
-      title="Income at which direct donation is preferable to $10/ton CO2",
-      fill="Inequality aversion"
-    )
-    plot_ylab = ylab("Yearly income (2020 USD)") 
+  plot_labs = labs(
+    title="Income at which direct donation is preferable to $10/ton CO2",
+    fill="Inequality aversion"
+  )
+  plot_ylab = ylab("Yearly income (2020 USD)") 
 }
 
 set.seed(10)
@@ -138,8 +198,9 @@ if (type_str == "eri_eq_statscc_2020d"){
     alpha=0.9, size=3
   ) + facet_grid(PRTP+eta~damages, labeller = label_axes) + 
     scale_y_continuous(trans = "log2") + scale_x_continuous(trans = "log2") + ylab(
-    "SCC inequality-indifferent value") + xlab("SCC inequality-averse value for Eritrea")
+      "SCC inequality-indifferent value") + xlab("SCC inequality-averse value for Eritrea")
   plotdif
   savediffig = paste0(type_str, "InequalityAlteration", version_string, ".png")
   ggsave(path="plots", filename=savediffig)
 }
+
