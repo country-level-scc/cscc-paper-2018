9954f8735a6c7a0a54296405525d2fd89703bf14
Lisanne <lisanne.blok19@imperial.ac.uk>
2021-07-26 15:23:47 +0200

    Corrected for name of data file and temperature change

--- modules/dice_replica.R
+++ modules/dice_replica.R
@@ -12,12 +12,12 @@
 # Dataset accessed 2021-07-22 at https://data.giss.nasa.gov/gistemp/
 # contains temperature anomalies in Celcius compared to mean from 1951-1980
 
-temp_csv = fread(file.path('data','GLB.Ts+dSST.csv'))
-temp_anomaly = select(temp_csv, "Year","D-N") # select yearly anomaly 
+temp_csv = fread(file.path('data','NASA_temp_change_1900-2020.csv'))
+temp_anomaly = select(temp_csv, "Year","J-D") # select yearly anomaly 
 anomaly_years <- c(1900,2020)
 temp_anomaly <- temp_anomaly[Year %in% anomaly_years] 
 # find increase of temperature from 1900 to 2020 by taking the difference of the anomalies
-temp_history <- as.numeric(temp_anomaly[Year == 2020, "D-N"][[1]]) - as.numeric(temp_anomaly[Year == 1900, "D-N"][[1]])
+temp_history <- as.numeric(temp_anomaly[Year == 2020, "J-D"][[1]]) - as.numeric(temp_anomaly[Year == 1900, "D-N"][[1]])
 
 warming_effect <- function(temp, temp_prev, gdp_tm1, nid, out_of_sample=T, temp_history){
   # temperature increase since 1900. temp_history is global T increase, from 2020 is national increase
